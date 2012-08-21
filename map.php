6bd5a89382295de0a583c9d7314a53be97bd07dc
Liu Zenan <liuzenan711@gmail.com>
2012-08-21 09:21:15 +0800

    add google maps

--- map.php
+++ map.php
@@ -4,4 +4,13 @@
 ?>
 
   <?php include 'include/navbar.php' ?>
+  <div class="map-wrap">
+    <div class="map-google">
+      <iframe class="map-content" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/?ie=UTF8&amp;t=m&amp;ll=26.431228,24.960938&amp;spn=123.207193,225&amp;z=2&amp;output=embed"></iframe>
+       </div>
+    <div class="friends-profile-wrap">
+      
+
+    </div>
+  </div>
 <?php include 'include/footer.php'?>
