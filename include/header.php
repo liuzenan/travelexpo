6544d0e96a462f0f72ee79f23589775a7ef0693b
Jiao Jing Ping <elleryjiao@gmail.com>
2012-08-22 00:09:39 +0800

    minor change

--- include/header.php
+++ include/header.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 include 'session.php';
- ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- index.php
+++ index.php
@@ -2,6 +2,45 @@
 $title = 'Explore travel destinations from your friends\' photos!';
 include 'include\header.php'
  ?>
+   <div id="fb-root"></div>
+        <script type="text/javascript">
+            window.fbAsyncInit = function() {
+                FB.init({appId: '334745573285940', status: true, cookie: true, xfbml: true});
+ 
+                /* All the events registered */
+                FB.Event.subscribe('auth.login', function(response) {
+                    // do something with response
+                    login();
+                });
+                FB.Event.subscribe('auth.logout', function(response) {
+                    // do something with response
+                    logout();
+                });
+ 
+                FB.getLoginStatus(function(response) {
+                    if (response.session) {
+                        // logged in and connected user, someone you know
+                        login();
+                    }
+                });
+            };
+            (function() {
+                var e = document.createElement('script');
+                e.type = 'text/javascript';
+                e.src = document.location.protocol +
+                    '//connect.facebook.net/en_US/all.js';
+                e.async = true;
+                document.getElementById('fb-root').appendChild(e);
+            }());
+ 
+            function login(){
+               window.location="map.php";
+            }
+            function logout(){
+              window.location="index.php";
+            }
+
+        </script>
  <div class="main-content">
         <header class="landing-header">
             <a href="" title="TravelExpo">TravelExpo is a facebook application that helps you to explore travel destinations through your friends travel photos.
@@ -17,7 +56,7 @@ include 'include\header.php'
                 <div class="span8">
                     <h1>Explore the best travel destinations</h1>
                     <p>through your friends facebook photos and make it more fun to plan future trips!</p>
-                    <p><a href="" class="btn btn-primary btn-large">Login in using Facebook</a></p>
+                    <p><fb:login-button autologoutlink="true" perms="email,user_birthday,status_update,publish_stream"></fb:login-button></p>
                 </div>
                
         </div>
